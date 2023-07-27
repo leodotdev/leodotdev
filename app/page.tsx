944e7491305c898264471f776a80dc18ba15229e
Leo SF <leo@leo.miami>
2023-07-27 10:24:17 -0400

    Initial commit

---
+++ app/page.tsx
@@ -0,0 +1,250 @@
+"use client";
+
+import Image from "next/image";
+import { motion, useScroll } from "framer-motion";
+
+import { RiPaintBrushFill, RiNodeTree } from "react-icons/ri";
+
+// const { scrollYProgress } = useScroll();
+
+export default function Home() {
+  return (
+    <main>
+      <div className="container p-12 lg:p-24">
+        <div className="mb-24 flex w-full flex-row items-center justify-between gap-4">
+          <div className="flex flex-col">
+            <h2 className={`text-2xl font-semibold text-stone-950`}>Leo</h2>
+            <p className={`max-w-[30ch] text-sm text-stone-500`}>
+              Software Designer
+            </p>
+          </div>
+          <div className="flex flex-row gap-4">
+            <motion.a
+              href="https://twitter.com/leosuccarferre"
+              target="_blank"
+              whileHover={{ scale: 1.25 }}
+              whileTap={{ scale: 1 }}
+              className="cursor-pointer rounded-full bg-stone-100 p-3 px-5 dark:bg-stone-800/30"
+            >
+              Twitter
+            </motion.a>
+            <motion.a
+              href="https://www.linkedin.com/in/leosuccarferre/"
+              target="_blank"
+              whileHover={{ scale: 1.25 }}
+              whileTap={{ scale: 1 }}
+              className="cursor-pointer rounded-full bg-stone-100 p-3 px-5 dark:bg-stone-800/30"
+            >
+              LinkedIn
+            </motion.a>
+          </div>
+        </div>
+
+        <div className="mb-24 flex flex-col gap-4">
+          <motion.div
+            whileHover={{ scale: 1.25 }}
+            whileTap={{ scale: 1 }}
+            className="flex cursor-pointer flex-row items-center gap-4 overflow-auto rounded-3xl bg-stone-100 p-8 transition-colors dark:border-stone-700 dark:bg-stone-800/30 lg:p-12"
+          >
+            {[
+              ["/starterkit1.png", "StarterKit"],
+              ["/starterkit2.png", "StarterKit"],
+              ["/starterkit3.png", "StarterKit"],
+              ["/starterkit4.png", "StarterKit"],
+            ].map(([file, alt]) => (
+              <Image
+                className="rounded"
+                src={file}
+                alt={alt}
+                width="800"
+                height="600"
+                objectFit="contain"
+                priority
+              />
+            ))}
+          </motion.div>
+          <div className="flex flex-row items-center gap-3">
+            <div className="rounded-full bg-stone-100 p-3 dark:bg-stone-800/30">
+              <RiPaintBrushFill
+                title="Visual Design"
+                className="h-5 w-5 fill-stone-500"
+              />
+            </div>
+            <div className="flex flex-col">
+              <h2 className="text-xl font-semibold text-stone-950">
+                StarterKit{" "}
+              </h2>
+              <p className="max-w-[30ch] text-sm text-stone-500">
+                Plasmic, 2023
+              </p>
+            </div>
+          </div>
+        </div>
+
+        <div className="mb-24 flex flex-col gap-4">
+          <motion.div
+            whileHover={{ scale: 1.25 }}
+            whileTap={{ scale: 1 }}
+            className="flex cursor-pointer flex-row items-center gap-4 overflow-auto rounded-3xl bg-stone-100 p-8 transition-colors dark:border-stone-700 dark:bg-stone-800/30 lg:p-12"
+          >
+            {[["/field-states-2021.svg", "Field States"]].map(
+              ([file, name]) => (
+                <Image
+                  className="rounded"
+                  src={file}
+                  alt={name}
+                  width="1600"
+                  height="1200"
+                  objectFit="contain"
+                  priority
+                />
+              ),
+            )}
+          </motion.div>
+
+          <div className="flex flex-row items-center gap-3">
+            <div className="rounded-full bg-stone-100 p-3 dark:bg-stone-800/30">
+              <RiNodeTree
+                title="Visual Design"
+                className="h-5 w-5 fill-stone-500"
+              />
+            </div>
+
+            <div className="flex flex-col">
+              <h2 className="text-xl font-semibold text-stone-950">
+                Field States
+              </h2>
+
+              <p className="max-w-[30ch] text-sm text-stone-500">
+                Plasmic, 2021
+              </p>
+            </div>
+          </div>
+        </div>
+
+        <div className="mb-24 flex flex-col gap-4">
+          <motion.div
+            whileHover={{ scale: 1.25 }}
+            whileTap={{ scale: 1 }}
+            className="flex cursor-pointer flex-row items-center gap-4 overflow-auto rounded-3xl bg-stone-100 p-8 transition-colors dark:border-stone-700 dark:bg-stone-800/30 lg:p-12"
+          >
+            {[["/variants-exploration-2021.svg", "Variants Panel"]].map(
+              ([file, name]) => (
+                <Image
+                  className="rounded"
+                  src={file}
+                  alt={name}
+                  width="1600"
+                  height="1200"
+                  objectFit="contain"
+                  priority
+                />
+              ),
+            )}
+          </motion.div>
+
+          <div className="flex flex-row items-center gap-3">
+            <div className="rounded-full bg-stone-100 p-3 dark:bg-stone-800/30">
+              <RiNodeTree
+                title="Visual Design"
+                className="h-5 w-5 fill-stone-500"
+              />
+            </div>
+
+            <div className="flex flex-col">
+              <h2 className="text-xl font-semibold text-stone-950">
+                Variants Panel
+              </h2>
+
+              <p className="max-w-[30ch] text-sm text-stone-500">
+                Plasmic, 2021
+              </p>
+            </div>
+          </div>
+        </div>
+
+        <div className="mb-24 flex flex-col gap-4">
+          <motion.div
+            whileHover={{ scale: 1.25 }}
+            whileTap={{ scale: 1 }}
+            className="flex cursor-pointer flex-row items-center justify-center gap-4 overflow-auto rounded-3xl bg-stone-100 p-8 transition-colors dark:border-stone-700 dark:bg-stone-800/30 lg:p-12"
+          >
+            {[["/chrome-wireframe-2023.svg", "Variants Panel"]].map(
+              ([file, name]) => (
+                <Image
+                  className="rounded"
+                  src={file}
+                  alt={name}
+                  width="800"
+                  height="600"
+                  objectFit="contain"
+                  priority
+                />
+              ),
+            )}
+          </motion.div>
+
+          <div className="flex flex-row items-center gap-3">
+            <div className="rounded-full bg-stone-100 p-3 dark:bg-stone-800/30">
+              <RiNodeTree
+                title="Visual Design"
+                className="h-5 w-5 fill-stone-500"
+              />
+            </div>
+
+            <div className="flex flex-col">
+              <h2 className="text-xl font-semibold text-stone-950">
+                Chrome Wireframe
+              </h2>
+
+              <p className="max-w-[30ch] text-sm text-stone-500">
+                Plasmic, 2023
+              </p>
+            </div>
+          </div>
+        </div>
+
+        <div className="mb-24 flex flex-col gap-4">
+          <motion.div
+            whileHover={{ scale: 1.25 }}
+            whileTap={{ scale: 1 }}
+            className="flex cursor-pointer flex-row items-center justify-center gap-4 overflow-auto rounded-3xl bg-stone-100 p-8 transition-colors dark:border-stone-700 dark:bg-stone-800/30 lg:p-12"
+          >
+            {[["/insert-menu-2023.svg", "Variants Panel"]].map(
+              ([file, name]) => (
+                <Image
+                  className="rounded"
+                  src={file}
+                  alt={name}
+                  width="800"
+                  height="600"
+                  objectFit="contain"
+                  priority
+                />
+              ),
+            )}
+          </motion.div>
+
+          <div className="flex flex-row items-center gap-3">
+            <div className="rounded-full bg-stone-100 p-3 dark:bg-stone-800/30">
+              <RiNodeTree
+                title="Visual Design"
+                className="h-5 w-5 fill-stone-500"
+              />
+            </div>
+
+            <div className="flex flex-col">
+              <h2 className="text-xl font-semibold text-stone-950">
+                Insert Menu
+              </h2>
+
+              <p className="max-w-[30ch] text-sm text-stone-500">
+                Plasmic, 2023
+              </p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </main>
+  );
+}
