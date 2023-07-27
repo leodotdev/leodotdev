75ee4997b8fcbaa8b4e5c769920c77dc8c5ad165
Leo SF <leo@leo.miami>
2023-07-27 11:57:20 -0400

    svg test

--- app/page.tsx
+++ app/page.tsx
@@ -14,9 +14,7 @@ export default function Home() {
         <div className="mb-24 flex w-full flex-row items-center justify-between gap-4">
           <div className="flex flex-col">
             <h2 className={`text-2xl font-semibold text-stone-950`}>Leo</h2>
-            <p className={`max-w-[30ch] text-sm text-stone-500`}>
-              Software Designer
-            </p>
+            <p className={`text-sm text-stone-500`}>Software Designer</p>
           </div>
           <div className="flex flex-row gap-4">
             <motion.a
@@ -47,10 +45,11 @@ export default function Home() {
             className="flex cursor-pointer flex-row items-center gap-4 overflow-auto rounded-3xl bg-stone-100 p-8 transition-colors dark:border-stone-700 dark:bg-stone-800/30 lg:p-12"
           >
             {[
-              ["/starterkit1.png", "StarterKit"],
-              ["/starterkit2.png", "StarterKit"],
-              ["/starterkit3.png", "StarterKit"],
-              ["/starterkit4.png", "StarterKit"],
+              ["/starterkit-2023-1.svg", "Plasmic StarterKit"],
+              ["/starterkit-2023-2.svg", "Plasmic StarterKit"],
+              ["/starterkit-2023-3.svg", "Plasmic StarterKit"],
+              ["/starterkit-2023-4.svg", "Plasmic StarterKit"],
+              ["/starterkit-2023-5.svg", "Plasmic StarterKit"],
             ].map(([file, alt]) => (
               <Image
                 className="rounded"
@@ -74,9 +73,7 @@ export default function Home() {
               <h2 className="text-xl font-semibold text-stone-950">
                 StarterKit{" "}
               </h2>
-              <p className="max-w-[30ch] text-sm text-stone-500">
-                Plasmic, 2023
-              </p>
+              <p className="text-sm text-stone-500">Plasmic, 2023</p>
             </div>
           </div>
         </div>
@@ -85,16 +82,16 @@ export default function Home() {
           <motion.div
             whileHover={{ scale: 1.25 }}
             whileTap={{ scale: 1 }}
-            className="flex cursor-pointer flex-row items-center gap-4 overflow-auto rounded-3xl bg-stone-100 p-8 transition-colors dark:border-stone-700 dark:bg-stone-800/30 lg:p-12"
+            className="flex cursor-pointer flex-row items-center justify-center gap-4 overflow-auto rounded-3xl bg-stone-100 p-8 transition-colors dark:border-stone-700 dark:bg-stone-800/30 lg:p-12"
           >
-            {[["/field-states-2021.svg", "Field States"]].map(
+            {[["/field-states-2021.svg", "Plasmic Field States"]].map(
               ([file, name]) => (
                 <Image
                   className="rounded"
                   src={file}
                   alt={name}
-                  width="1600"
-                  height="1200"
+                  width="800"
+                  height="600"
                   objectFit="contain"
                   priority
                 />
@@ -115,9 +112,7 @@ export default function Home() {
                 Field States
               </h2>
 
-              <p className="max-w-[30ch] text-sm text-stone-500">
-                Plasmic, 2021
-              </p>
+              <p className="text-sm text-stone-500">Plasmic, 2021</p>
             </div>
           </div>
         </div>
@@ -126,7 +121,7 @@ export default function Home() {
           <motion.div
             whileHover={{ scale: 1.25 }}
             whileTap={{ scale: 1 }}
-            className="flex cursor-pointer flex-row items-center gap-4 overflow-auto rounded-3xl bg-stone-100 p-8 transition-colors dark:border-stone-700 dark:bg-stone-800/30 lg:p-12"
+            className="flex cursor-pointer flex-row items-center justify-center gap-4 overflow-auto rounded-3xl bg-stone-100 p-8 transition-colors dark:border-stone-700 dark:bg-stone-800/30 lg:p-12"
           >
             {[["/variants-exploration-2021.svg", "Variants Panel"]].map(
               ([file, name]) => (
@@ -134,8 +129,8 @@ export default function Home() {
                   className="rounded"
                   src={file}
                   alt={name}
-                  width="1600"
-                  height="1200"
+                  width="800"
+                  height="600"
                   objectFit="contain"
                   priority
                 />
@@ -156,9 +151,7 @@ export default function Home() {
                 Variants Panel
               </h2>
 
-              <p className="max-w-[30ch] text-sm text-stone-500">
-                Plasmic, 2021
-              </p>
+              <p className="text-sm text-stone-500">Plasmic, 2021</p>
             </div>
           </div>
         </div>
@@ -169,7 +162,7 @@ export default function Home() {
             whileTap={{ scale: 1 }}
             className="flex cursor-pointer flex-row items-center justify-center gap-4 overflow-auto rounded-3xl bg-stone-100 p-8 transition-colors dark:border-stone-700 dark:bg-stone-800/30 lg:p-12"
           >
-            {[["/chrome-wireframe-2023.svg", "Variants Panel"]].map(
+            {[["/chrome-wireframe-2023.svg", "Plasmic Variants Panel"]].map(
               ([file, name]) => (
                 <Image
                   className="rounded"
@@ -197,9 +190,7 @@ export default function Home() {
                 Chrome Wireframe
               </h2>
 
-              <p className="max-w-[30ch] text-sm text-stone-500">
-                Plasmic, 2023
-              </p>
+              <p className="text-sm text-stone-500">Plasmic, 2023</p>
             </div>
           </div>
         </div>
@@ -238,9 +229,43 @@ export default function Home() {
                 Insert Menu
               </h2>
 
-              <p className="max-w-[30ch] text-sm text-stone-500">
-                Plasmic, 2023
-              </p>
+              <p className="text-sm text-stone-500">Plasmic, 2023</p>
+            </div>
+          </div>
+        </div>
+
+        <div className="mb-24 flex flex-col gap-4">
+          <motion.div
+            whileHover={{ scale: 1.25 }}
+            whileTap={{ scale: 1 }}
+            className="flex cursor-pointer flex-row items-center gap-4 overflow-auto rounded-3xl bg-stone-100 p-8 transition-colors dark:border-stone-700 dark:bg-stone-800/30 lg:p-12"
+          >
+            {[
+              ["/copilot-2023-1.svg", "Plasmic Copilot"],
+              ["/copilot-2023-2.svg", "Plasmic Copilot"],
+              ["/copilot-2023-3.svg", "Plasmic Copilot"],
+            ].map(([file, alt]) => (
+              <Image
+                className="rounded"
+                src={file}
+                alt={alt}
+                width="800"
+                height="600"
+                objectFit="contain"
+                priority
+              />
+            ))}
+          </motion.div>
+          <div className="flex flex-row items-center gap-3">
+            <div className="rounded-full bg-stone-100 p-3 dark:bg-stone-800/30">
+              <RiPaintBrushFill
+                title="Visual Design"
+                className="h-5 w-5 fill-stone-500"
+              />
+            </div>
+            <div className="flex flex-col">
+              <h2 className="text-xl font-semibold text-stone-950">Copilot </h2>
+              <p className="text-sm text-stone-500">Plasmic, 2023</p>
             </div>
           </div>
         </div>
