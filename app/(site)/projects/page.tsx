b21c21791801cba50f27503728d4ad97a613aa0e
Leo SF <leo@leo.miami>
2024-08-19 11:46:35 -0400

    dark theme bg fix

--- app/(site)/projects/page.tsx
+++ app/(site)/projects/page.tsx
@@ -224,7 +224,7 @@ export default async function Home() {
         <div className="text-stone-500">Work and employment history.</div>
       </div>
 
-      <div className="-mx-[1px] rounded-3xl border bg-white">
+      <div className="-mx-[1px] rounded-3xl border bg-white dark:bg-black">
         <ul className="flex flex-col">
           <li className="flex w-full items-center justify-between p-6 md:px-12 md:pt-12 ">
             <div className="flex flex-row items-center gap-2">
@@ -381,7 +381,7 @@ export default async function Home() {
           </li>
         </ul>
 
-        {/* <Card className="rounded-3xl bg-white/50 shadow-none dark:bg-black/50">
+        {/* <Card className="rounded-3xl bg-white dark:bg-black/50 shadow-none dark:bg-black/50">
           <CardHeader>
             <CardTitle className="text-lg">Favorite Tools</CardTitle>
           </CardHeader>
@@ -417,7 +417,7 @@ export default async function Home() {
         <CarouselPrevious />
         <CarouselContent>
           <CarouselItem>
-            <div className="flex flex-col gap-4 rounded-3xl border bg-white p-6 md:p-12">
+            <div className="flex flex-col gap-4 rounded-3xl border bg-white p-6 dark:bg-black md:p-12">
               <blockquote className="flex flex-col gap-4">
                 <p>
                   &#34;Leo volunteered to design for the Sticker Admin Tool for
@@ -441,7 +441,7 @@ export default async function Home() {
             </div>
           </CarouselItem>
           <CarouselItem>
-            <div className="flex flex-col gap-4 rounded-3xl border bg-white p-6 md:p-12">
+            <div className="flex flex-col gap-4 rounded-3xl border bg-white p-6 dark:bg-black md:p-12">
               <blockquote className="text-xl">
                 &#34;Leo is a deep thinker who lives in-between design and code.
                 While at BitGo, Leo took ownership of the design system by
@@ -461,7 +461,7 @@ export default async function Home() {
             </div>
           </CarouselItem>
           <CarouselItem>
-            <div className="flex flex-col gap-4 rounded-3xl border bg-white p-6 md:p-12">
+            <div className="flex flex-col gap-4 rounded-3xl border bg-white p-6 dark:bg-black md:p-12">
               <blockquote className="text-xl">
                 &#34;Rarely do you meet a creative with such a lethal
                 combination of technical skills and fundamental design ability.
@@ -476,7 +476,7 @@ export default async function Home() {
             </div>
           </CarouselItem>
           <CarouselItem>
-            <div className="flex flex-col gap-4 rounded-3xl border bg-white p-6 md:p-12">
+            <div className="flex flex-col gap-4 rounded-3xl border bg-white p-6 dark:bg-black md:p-12">
               <blockquote className="text-xl">
                 &#34;I can&#39;t stress the relief I felt every time Leo and I
                 worked together. He is one of the most reliable partners and
@@ -489,7 +489,7 @@ export default async function Home() {
             </div>
           </CarouselItem>
           <CarouselItem>
-            <div className="flex flex-col gap-4 rounded-3xl border bg-white p-6 md:p-12">
+            <div className="flex flex-col gap-4 rounded-3xl border bg-white p-6 dark:bg-black md:p-12">
               <blockquote className="text-xl">
                 &#34;You are all killing it on the design front! Thank you so
                 much for your hard work and always pushing for what&#39;s best
@@ -547,7 +547,7 @@ export default async function Home() {
         {projects.map((project) => (
           // project card
           <Dialog key={project._id}>
-            <DialogTrigger className="group -mx-[1px] flex flex-col items-stretch justify-between gap-4 overflow-clip rounded-3xl border bg-white p-6 md:p-12">
+            <DialogTrigger className="group -mx-[1px] flex flex-col items-stretch justify-between gap-4 overflow-clip rounded-3xl border bg-white p-6 dark:bg-black md:p-12">
               <div className="flex items-end justify-between">
                 <div className="flex flex-col text-left text-stone-950 dark:text-stone-50">
                   <div className="font-medium">{project.name}</div>
